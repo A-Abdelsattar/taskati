576c451a0c9f34399a9a6b234c9e850309004af9
A-Abdelsattar <ahmedabdelsattar28@gmail.com>
2025-07-05 11:44:58 +0300

    splash screen done and add screen utils

--- lib/features/splash/splash_screen.dart
+++ lib/features/splash/splash_screen.dart
@@ -1,12 +1,57 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:taskati/features/home/home_screen.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(Duration(seconds:1),(){
+
+     Navigator.push(context, MaterialPageRoute(builder: (context)=>HomeScreen()));
+    });
+    super.initState();
+  }
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            Image.asset("assets/images/splash_logo.gif",
+
+            ),
+            SizedBox(height: 20.h,),
+            Text("Taskati",style: TextStyle(
+              fontSize: 20.sp,
+              fontWeight: FontWeight.bold
+            ),),
+            SizedBox(height: 30.h,),
+            Text("it's time to get organized",
+            style: TextStyle(
+              fontSize: 17.sp,
+
+            ),
+
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/taskati_app.dart
+++ lib/taskati_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:taskati/features/splash/splash_screen.dart';
 
 class TaskatiApp extends StatelessWidget {
@@ -6,9 +7,14 @@ class TaskatiApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+    return ScreenUtilInit(
+      designSize: const Size(390, 844),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SplashScreen(),
+      ),
     );
   }
 }
